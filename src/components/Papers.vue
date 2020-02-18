7da9bf68b10332383a52edaabaf9f637314ac148
Coding Unicorn <co.unicorn.ding@gmail.com>
2020-02-18 12:44:28 +0200

    call for papers -> cfp

--- src/components/Papers.vue
+++ src/components/Papers.vue
@@ -1,12 +1,12 @@
 <template>
   <span>
     <router-link v-if="!$route.params.cfp" :to="route('events', { cfp: 'cfp' })"
-      ><i class="far fa-circle"></i> with call for papers</router-link
+      ><i class="far fa-circle"></i> with cfp</router-link
     >
     <router-link
       v-if="$route.params.cfp"
       :to="route('events', { cfp: undefined })"
-      ><i class="far fa-check-circle"></i> with call for papers</router-link
+      ><i class="far fa-check-circle"></i> with cfp</router-link
     >
   </span>
 </template>
