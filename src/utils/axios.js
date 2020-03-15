8b21670aa170c1e44d060c7b7edd9f998078ff6c
Coding Unicorn <co.unicorn.ding@gmail.com>
2020-03-15 21:08:22 +0700

    no console.log on production

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,6 @@
 import * as globalAxios from "axios";
 
 export default (jwtToken = undefined) => {
-  console.log(jwtToken);
   const newAxios = globalAxios.create();
   newAxios.interceptors.request.use(config => {
     config.headers.authorization = jwtToken;
