028ff5b79cc462ea9d924d033634684fde1b1d14
Coding Unicorn <co.unicorn.ding@gmail.com>
2020-06-05 12:15:13 +0700

    allow multiple chars enter during topic search

--- src/components/NewEvent.vue
+++ src/components/NewEvent.vue
@@ -30,7 +30,6 @@
                   >
                     <template #search="{ attributes, events }">
                       <input
-                        maxlength="1"
                         class="vs__search"
                         v-bind="attributes"
                         v-on="events"
